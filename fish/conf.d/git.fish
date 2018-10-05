ab5f08cd55af17bc3a47ae777936bcb0329a6718
Alan Ly <me@alanly.ca>
2018-10-05 16:22:16 -0400

    Update git.fish

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -1,6 +1,7 @@
 # Aliases for 'git'
 alias g="git"
 alias grm="g fetch origin master; and g rebase origin/master"
+alias gcl="g br --merged | grep --invert-match 'master' | xargs git br -d"
 
 function gco
   g fetch origin $argv; and g checkout $argv
