0333ffe95c6a8531a96738210c467932c6729098
Alan Ly <me@alanly.ca>
2021-06-23 12:03:22 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,17 @@ if [ $SPIN ]; then
     vim
     
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --profile default --no-modify-path -y
+  source ~/.cargo/env
 
   cargo install \
     lsd \
     ripgrep \
     starship \
     zoxide
+    
+  ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+  ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+  ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 fi
