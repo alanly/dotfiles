e799480a5fed3035e45f8659e2ee38b08d8bc08f
Alan Ly <me@alanly.ca>
2021-07-05 13:36:39 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,22 +3,36 @@
 # Spin-specific bootstrapping
 if [ $SPIN ]; then
 
+  # Install Ubuntu packages
   sudo apt-get install -y \
     build-essential \
     htop \
     vim
     
+  # Install Rustup
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --profile default --no-modify-path -y
 
+  # Install packages from Cargo
   ~/.cargo/bin/cargo install \
     lsd \
     ripgrep \
     starship \
     zoxide
     
+  # Link Starship configuration
   ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+  
+  # Link Git configuration
   ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+  
+  # Link Vim configuration
   ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  
+  # Link Zsh configuration
   ln -sf ~/dotfiles/.zshrc ~/.zshrc
+  
+  # Link modified Spin `server` command for Core
+  ln -sf ~/dotfiles/spin-bin/core-server.sh ~/.spin/bin/server
+  chmod +x ~/.spin/bin/server
 
 fi
