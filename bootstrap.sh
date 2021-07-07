9c622b34f4602975604086244aca460b221c85a0
Alan Ly <me@alanly.ca>
2021-07-07 14:02:13 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,18 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 # Spin-specific bootstrapping
 if [ $SPIN ]; then
 
-  # Install Ubuntu packages
-  sudo apt-get install -y \
-    build-essential \
-    htop \
-    vim
-    
-  # Install Rustup
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --profile default --no-modify-path -y
-
-  # Install packages from Cargo
-  ~/.cargo/bin/cargo install \
-    lsd \
-    ripgrep \
-    starship \
-    zoxide
-    
-  # Link Starship configuration
-  ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+  # Install dependencies
+  bash ~/dotfiles/spin-scripts/install-native-packages.sh
+  bash ~/dotfiles/spin-scripts/install-rust-packages.sh
   
-  # Link Git configuration
-  ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+  # Symlink configs
+  bash ~/dotfiles/spin-scripts/link-configs.sh
   
-  # Link Vim configuration
-  ln -sf ~/dotfiles/.vimrc ~/.vimrc
-  
-  # Link Zsh configuration
-  ln -sf ~/dotfiles/.zshrc ~/.zshrc
-  
-  # Do `shopify` specific things.
+  # Bootstrap `shopify` specific things
   if [ $SPIN_SHOPIFY_SERVICE_FQDN ]; then
-    bash ~/dotfiles/spin-scripts/hedwig.sh
+    bash ~/dotfiles/spin-scripts/boot-hedwig-workers.sh
   fi
 
 fi
