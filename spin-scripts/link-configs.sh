30d2533aca47e1429b509a4f4f6c0eaa0d5ee4dc
Alan Ly <me@alanly.ca>
2021-07-07 14:00:57 -0400

    Create link-configs.sh

---
+++ spin-scripts/link-configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Link Starship configuration
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+
+# Link Git configuration
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# Link Vim configuration
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# Link Zsh configuration
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
