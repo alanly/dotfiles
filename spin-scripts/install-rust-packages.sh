3a2692a0b229cfaa8116c163c37225600fa0b98e
Alan Ly <me@alanly.ca>
2022-05-19 15:38:10 -0400

    Update install-rust-packages.sh

--- spin-scripts/install-rust-packages.sh
+++ spin-scripts/install-rust-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --profile default --no-modify-path -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --default-toolchain stable --no-modify-path -y
 
 # Install packages from Cargo
 ~/.cargo/bin/cargo install \
